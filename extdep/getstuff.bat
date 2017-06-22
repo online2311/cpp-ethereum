2c59776aed8fcb9ee8a60c5adfac09a809623ede
online2311 <13821320100@outlook.com>
2017-06-22 17:07:26 +0800

    Update getstuff.bat

--- extdep/getstuff.bat
+++ extdep/getstuff.bat
@@ -3,7 +3,7 @@ if not exist download mkdir download
 if not exist install mkdir install
 if not exist install\windows mkdir install\windows
 
-set eth_server=https://build.ethdev.com/builds/windows-precompiled
+set eth_server=https://github.com/ethereum/cpp-dependencies/releases/download/vc120/
 
 call :download boost 1.55.0
 call :download cryptopp 5.6.2
